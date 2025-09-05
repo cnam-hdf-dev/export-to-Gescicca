7fe8d31536b9fefd56ab8a8d4762d3a26ebd3527
cnam_hdf_dev <jonathan.cayrol@lecnam.net>
2025-09-05 15:35:38 +0200

    amelioration script : ajout champ formation, selection des centres, csv de ref pour nationalite, commune de naissance et pays de naissance

--- src/components/ExportApprenants.jsx
+++ src/components/ExportApprenants.jsx
@@ -1,253 +1,65 @@
 import { useState, useEffect } from "react";
 import "./ExportApprenants.css";
+import Papa from "papaparse";
 
 const token = process.env.REACT_APP_API_TOKEN;
+
+let communesMap = {};
+let nationalitesMap = {};
+let paysMap = {};
+
+const loadReferenceCsv = async (filePath, keyField, valueField) => {
+  return new Promise((resolve) => {
+    Papa.parse(filePath, {
+      header: true,
+      download: true,
+      delimiter: ";",
+      complete: (results) => {
+        const map = {};
+        results.data.forEach((row) => {
+          if (row[keyField] && row[valueField]) {
+            map[row[keyField]] = row[valueField];
+          }
+        });
+        console.log(map);
+        resolve(map);
+      },
+    });
+  });
+};
 
 export default function ExportApprenants() {
   const [groupes, setGroupes] = useState([]);
   const [selectedGroupe, setSelectedGroupe] = useState("");
+  const [nomFormation, setNomFormation] = useState("");
+  const [nomCentreEnseignement, setNomCentreEnseignement] = useState("");
+  const [nomCentreAttachement, setNomCentreAttachement] = useState("");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch(`/api/r/v1/formation-longue/groupes`, {
-      headers: {
-        "X-Auth-Token": token,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const liste = Object.values(data);
-        setGroupes(liste);
-      })
-      .catch((err) => console.error("Erreur chargement groupes:", err));
-  }, []);
+    const fetchData = async () => {
+      try {
+        const groupesRes = await fetch(`/api/r/v1/formation-longue/groupes`, {
+          headers: { "X-Auth-Token": token },
+        });
+        const groupesData = await groupesRes.json();
+        setGroupes(Object.values(groupesData));
 
-  const nationaliteMap = {
-    "France": 0,
-    "Acores-Madère": 319,
-    "Afghanistan": 212,
-    "Afrique du Sud": 303,
-    "Albanie": 125,
-    "Algérie": 352,
-    "Allemagne": 109,
-    "Andorre": 130,
-    "Angola": 395,
-    "Antigua-et-Barbuda": 441,
-    "Arabie Saoudite": 201,
-    "Argentine": 415,
-    "Arménie": 252,
-    "Australie": 501,
-    "Autriche": 110,
-    "Azerbaïdjan": 253,
-    "Bahamas": 436,
-    "Bahreïn": 249,
-    "Bangladesh": 246,
-    "Barbade": 434,
-    "Belgique": 131,
-    "Belize": 429,
-    "Bénin": 327,
-    "Bhoutan": 214,
-    "Biélorussie": 148,
-    "Birmanie": 224,
-    "Bolivie": 418,
-    "Bosnie-Herzégovine": 118,
-    "Botswana": 347,
-    "Brésil": 416,
-    "Brunei": 225,
-    "Bulgarie": 111,
-    "Burkina": 331,
-    "Burundi": 321,
-    "Cambodge": 234,
-    "Cameroun": 322,
-    "Canada": 401,
-    "Cap-Vert": 396,
-    "Chili": 417,
-    "Chine": 216,
-    "Chypre": 254,
-    "Colombie": 419,
-    "Comores": 397,
-    "Congo": 324,
-    "Congo (République Démocratique)": 312,
-    "Corée du Nord": 238,
-    "Corée du Sud": 239,
-    "Costa Rica": 406,
-    "Côte-d'Ivoire": 326,
-    "Croatie": 119,
-    "Cuba": 407,
-    "Danemark": 101,
-    "Djibouti": 399,
-    "Dominicaine (Rep. ou St-Domingue)": 408,
-    "Dominique (île de la)": 438,
-    "Egypte": 301,
-    "El Salvador": 414,
-    "Emirats Arabes Unis": 247,
-    "Equateur": 420,
-    "Erythrée": 317,
-    "Espagne": 134,
-    "Estonie": 106,
-    "Eswatini": 391,
-    "Etats-Unis d'Amérique": 404,
-    "Ethiopie": 315,
-    "Fidji": 508,
-    "Finlande": 105,
-    "Gabon": 328,
-    "Gambie": 304,
-    "Géorgie": 255,
-    "Ghana": 329,
-    "Gibraltar": 133,
-    "Grèce": 126,
-    "Grenade": 435,
-    "Guatemala": 409,
-    "Guinée": 330,
-    "Guinée Equatoriale": 314,
-    "Guinée-Bissau": 392,
-    "Guyana": 428,
-    "Haïti": 410,
-    "Honduras": 411,
-    "Hongkong": 230,
-    "Hongrie": 112,
-    "Inde": 223,
-    "Indonésie": 231,
-    "Irak": 203,
-    "Iran": 204,
-    "Irlande": 136,
-    "Islande": 102,
-    "Israël": 207,
-    "Italie": 127,
-    "Jamaïque": 426,
-    "Japon": 217,
-    "Jordanie": 222,
-    "Kazakhstan": 256,
-    "Kenya": 332,
-    "Kirghizistan": 257,
-    "Kiribati": 513,
-    "Kosovo": 157,
-    "Koweït": 240,
-    "Laos": 241,
-    "Lesotho": 348,
-    "Lettonie": 107,
-    "Liban": 205,
-    "Libéria": 302,
-    "Libye": 316,
-    "Liechtenstein": 113,
-    "Lituanie": 108,
-    "Luxembourg": 137,
-    "Macao": 232,
-    "Macédoine": 156,
-    "Madagascar": 333,
-    "Malaisie": 227,
-    "Malawi": 334,
-    "Maldives": 229,
-    "Mali": 335,
-    "Malte": 144,
-    "Maroc": 350,
-    "Marshall (îles)": 515,
-    "Maurice": 390,
-    "Mauritanie": 336,
-    "Mexique": 405,
-    "Micronésie": 516,
-    "Moldavie": 151,
-    "Monaco": 138,
-    "Mongolie": 242,
-    "Monténégro": 120,
-    "Mozambique": 393,
-    "Namibie": 311,
-    "Nauru": 507,
-    "Népal": 215,
-    "Nicaragua": 412,
-    "Niger": 337,
-    "Nigeria": 338,
-    "Non mentionnée": 990,
-    "Norvège": 103,
-    "Nouvelle-Zélande": 502,
-    "Oman": 250,
-    "Ouganda": 339,
-    "Ouzbékistan": 258,
-    "Pakistan": 213,
-    "Palaos (Iles)": 517,
-    "Palestine (Etat de)": 261,
-    "Panama": 413,
-    "Papouasie Nouvelle Guinée": 510,
-    "Paraguay": 421,
-    "Pays-Bas": 135,
-    "Pérou": 422,
-    "Philippines": 220,
-    "Pologne": 122,
-    "Portugal": 139,
-    "Qatar": 248,
-    "République Centrafricaine": 323,
-    "République Tchèque": 116,
-    "Roumanie": 114,
-    "Royaume-Uni": 132,
-    "Russie": 123,
-    "Rwanda": 340,
-    "Sahara Occidental": 389,
-    "Saint-Christophe-et-Niévès": 442,
-    "Sainte-Hélène": 306,
-    "Sainte-Lucie (île)": 439,
-    "Saint-Martin": 128,
-    "Saint-Vincent (île)": 440,
-    "Salomon": 512,
-    "Samoa Occidentales": 506,
-    "Sans nationalité": 995,
-    "Sao tome-et-Principe": 394,
-    "Sénégal": 341,
-    "Serbie": 121,
-    "Seychelles": 398,
-    "Sierra Leone": 342,
-    "Singapour": 226,
-    "Slovaquie": 117,
-    "Slovénie": 145,
-    "Somalie": 318,
-    "Soudan": 343,
-    "Soudan du Sud": 349,
-    "Sri Lanka": 235,
-    "Suède": 104,
-    "Suisse": 140,
-    "Suriname": 437,
-    "Syrie": 206,
-    "Tadjikistan": 259,
-    "Taiwan": 236,
-    "Tanzanie": 309,
-    "Tchad": 344,
-    "Thaïlande": 219,
-    "Timor Oriental": 262,
-    "Togo": 345,
-    "Tonga ou Friendly": 509,
-    "Trinité-et-Tobago": 433,
-    "Tunisie": 351,
-    "Turkménistan": 260,
-    "Turquie": 208,
-    "Tuvalu": 511,
-    "Ukraine": 155,
-    "Uruguay": 423,
-    "Vanuatu": 514,
-    "Vatican": 129,
-    "Venezuela": 424,
-    "Vietnam": 243,
-    "Yémen": 251,
-    "Zambie": 346,
-    "Zimbabwe": 310
-  };
+        communesMap = await loadReferenceCsv("/ref/liste_communes.csv", "CODE_COMMUNE", "NOMENCL_INSEE");
+        nationalitesMap = await loadReferenceCsv("/ref/liste_nationalites.csv", "CODE_NATIONALITE", "ID_BASE_EXTERNE");
+        paysMap = await loadReferenceCsv("/ref/liste_pays.csv", "CODE_PAYS", "ID_BASE_EXTERNE");
+      } catch (err) {
+        console.error("Erreur lors du chargement initial :", err);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleExport = async () => {
     const groupeInfo = groupes.find(g => g.codeGroupe.toString() === selectedGroupe);
     const nomGroupe = groupeInfo?.nomGroupe || '';
-    let nomFormation = '';
-    if (groupeInfo?.codeFormation) {
-      try {
-        const formationRes = await fetch(`/api/r/v1/formations/${groupeInfo.codeFormation}`, {
-          headers: {
-            "X-Auth-Token": token,
-          },
-        });
-        const formationData = await formationRes.json();
-        nomFormation = formationData.nomFormation || groupeInfo.codeFormation;
-      } catch (e) {
-        console.error("Erreur récupération nom de formation:", e);
-        nomFormation = groupeInfo.codeFormation;
-      }
-    }
+
     if (!selectedGroupe) return;
     setLoading(true);
     try {
@@ -261,15 +73,15 @@ export default function ExportApprenants() {
       const csvRows = [
         [
           "TITRE",
-          "INDEMNISATION",
-          "EXPERIENCE",
+          // "INDEMNISATION",
+          // "EXPERIENCE",
           "CODE_NATIONALITE",
-          "SITUATION_FAMILIALE",
-          "PROFESSION_INSEE",
-          "ANCIEN_CODE_AUDITEUR",
-          "NUMERO_SECURITE_SOCIALE",
-          "INE",
-          "INE_CNAM",
+          // "SITUATION_FAMILIALE",
+          // "PROFESSION_INSEE",
+          // "ANCIEN_CODE_AUDITEUR",
+          // "NUMERO_SECURITE_SOCIALE",
+          // "INE",
+          // "INE_CNAM",
           "NOM",
           "PRENOM",
           "NOM_USAGE",
@@ -283,52 +95,52 @@ export default function ExportApprenants() {
           "PAYS",
           "TELEPHONE_PERSONNEL",
           "TELEPHONE_PORTABLE",
-          "TELEPHONE_PROFESSIONNEL",
-          "NUMERO_POSTE",
+          // "TELEPHONE_PROFESSIONNEL",
+          // "NUMERO_POSTE",
           "COURRIEL_PERSONNEL",
-          "PROFESSION",
-          "FONCTION",
-          "DATE_CESSATION_ACTIVITE",
-          "MEMO",
-          "COURRIEL_PROFESSIONNEL",
-          "ADRESSE_SECONDAIRE_1",
-          "ADRESSE_SECONDAIRE_2",
-          "CODE_POSTAL_SECONDAIRE",
-          "VILLE_SECONDAIRE",
-          "PAYS_SECONDAIRE",
-          "COMPTE_AUXILLIAIRE",
-          "RAISON_SOCIALE_ENTREPRISE",
-          "SIRET_ENTREPRISE",
-          "IDENTIFIANT_ENTREPRISE",
+          // "PROFESSION",
+          // "FONCTION",
+          // "DATE_CESSATION_ACTIVITE",
+          // "MEMO",
+          // "COURRIEL_PROFESSIONNEL",
+          // "ADRESSE_SECONDAIRE_1",
+          // "ADRESSE_SECONDAIRE_2",
+          // "CODE_POSTAL_SECONDAIRE",
+          // "VILLE_SECONDAIRE",
+          // "PAYS_SECONDAIRE",
+          // "COMPTE_AUXILLIAIRE",
+          // "RAISON_SOCIALE_ENTREPRISE",
+          // "SIRET_ENTREPRISE",
+          // "IDENTIFIANT_ENTREPRISE",
           "ANNEE",
           "CENTRE_ENSEIGNEMENT",
           "CENTRE_ATTACHEMENT",
-          "STATUT_AUDITEUR",
-          "CENTRE_REGIONAL_INSCRIPTEUR",
-          "DIPLOME",
-          "SITUATION_GEOGRAPHIQUE",
-          "DISPOSITIF_FINANCEMENT",
+          // "STATUT_AUDITEUR",
+          // "CENTRE_REGIONAL_INSCRIPTEUR",
+          // "DIPLOME",
+          // "SITUATION_GEOGRAPHIQUE",
+          // "DISPOSITIF_FINANCEMENT",
           "TYPE_FINANCEMENT_INSCRIPTION",
-          "TYPE_TARIF_INSCRIPTION",
-          "SITUATION_PROFESSIONNELLE",
-          "STATUT_EMPLOI",
-          "INSCRIT_POLE_EMPLOI",
-          "COMPTE_ANALYTIQUE",
+          // "TYPE_TARIF_INSCRIPTION",
+          // "SITUATION_PROFESSIONNELLE",
+          // "STATUT_EMPLOI",
+          // "INSCRIT_POLE_EMPLOI",
+          // "COMPTE_ANALYTIQUE",
           "FORMATION",
-          "ANNEE_FORMATION",
-          "OPTION_FORMATION",
+          // "ANNEE_FORMATION",
+          // "OPTION_FORMATION",
           "GROUPE_FORMATION",
           "STATUT_INSCRIPTION",
           "TYPE_INSCRIPTION",
-          "CODE_UNITE",
-          "GROUPE_UNITE",
-          "SEMESTRE_UNITE",
-          "TYPE_FINANCEMENT_UNITE",
-          "TYPE_TARIF_UNITE",
-          "CENTRE_REGIONAL_ORGANISATEUR",
-          "NOTE_SESSION_1",
-          "NOTE_SESSION_2",
-          "DATE_INSCRIPTION"
+          // "CODE_UNITE",
+          // "GROUPE_UNITE",
+          // "SEMESTRE_UNITE",
+          // "TYPE_FINANCEMENT_UNITE",
+          // "TYPE_TARIF_UNITE",
+          // "CENTRE_REGIONAL_ORGANISATEUR",
+          // "NOTE_SESSION_1",
+          // "NOTE_SESSION_2",
+          // "DATE_INSCRIPTION"
         ].join(";")
       ];
 
@@ -345,21 +157,21 @@ export default function ExportApprenants() {
       apprenantDetails.forEach((d) => {
         csvRows.push([
           d.codeCivilite, // TITRE
-          '',// INDEMNISATION
-          '',// EXPERIENCE
-          d.codeNationalite, // CODE_NATIONALITE
-          '',// SITUATION_FAMILIALE
-          '',// PROFESSION_INSEE
-          '',// ANCIEN_CODE_AUDITEUR
-          '',// NUMERO_SECURITE_SOCIALE
-          '',// INE
-          '',// INE_CNAM
+          // '',// INDEMNISATION
+          // '',// EXPERIENCE
+          nationalitesMap[d.codeNationalite] || "", // CODE_NATIONALITE
+          // '',// SITUATION_FAMILIALE
+          // '',// PROFESSION_INSEE
+          // '',// ANCIEN_CODE_AUDITEUR
+          // '',// NUMERO_SECURITE_SOCIALE
+          // '',// INE
+          // '',// INE_CNAM
           d.nomApprenant, // NOM
           d.prenomApprenant, // PRENOM
           d.nomJeuneFille || '',// NOM_USAGE
           d.dateNaissance, // DATE_NAISSANCE
-          d.codePaysNaissance, // PAYS_NAISSANCE
-          d.lieuNaissance, // LIEU_NAISSANCE
+          paysMap[d.codePaysNaissance] || "", // PAYS_NAISSANCE
+          communesMap[d.codeCommuneNaissance] || "", // LIEU_NAISSANCE
           d.adresse?.adr1, // ADRESSE_1
           d.adresse?.adr2 || '', // ADRESSE_2
           d.adresse?.cp, // CODE_POSTAL
@@ -367,52 +179,52 @@ export default function ExportApprenants() {
           d.adresse?.pays?.nomPays, // PAYS
           d.adresse?.tel1, // TELEPHONE_PERSONNEL
           d.adresse?.tel2, // TELEPHONE_PORTABLE
-          '',// TELEPHONE_PROFESSIONNEL
-          '',// NUMERO_POSTE
+          // '',// TELEPHONE_PROFESSIONNEL
+          // '',// NUMERO_POSTE
           d.adresse?.email, // COURRIEL_PERSONNEL
-          '',// PROFESSION
-          '',// FONCTION
-          '',// DATE_CESSATION_ACTIVITE
-          '',// MEMO
-          '',// COURRIEL_PROFESSIONNEL
-          '',// ADRESSE_SECONDAIRE_1
-          '',// ADRESSE_SECONDAIRE_2
-          '',// CODE_POSTAL_SECONDAIRE
-          '',// VILLE_SECONDAIRE
-          '',// PAYS_SECONDAIRE
-          '',// COMPTE_AUXILLIAIRE
-          '',// RAISON_SOCIALE_ENTREPRISE
-          '',// SIRET_ENTREPRISE
-          '',// IDENTIFIANT_ENTREPRISE
-          '',// ANNEE
-          '',// CENTRE_ENSEIGNEMENT
-          '',// CENTRE_ATTACHEMENT
-          '',// STATUT_AUDITEUR
-          '',// CENTRE_REGIONAL_INSCRIPTEUR
-          '',// DIPLOME
-          '',// SITUATION_GEOGRAPHIQUE
-          '',// DISPOSITIF_FINANCEMENT
+          // '',// PROFESSION
+          // '',// FONCTION
+          // '',// DATE_CESSATION_ACTIVITE
+          // '',// MEMO
+          // '',// COURRIEL_PROFESSIONNEL
+          // '',// ADRESSE_SECONDAIRE_1
+          // '',// ADRESSE_SECONDAIRE_2
+          // '',// CODE_POSTAL_SECONDAIRE
+          // '',// VILLE_SECONDAIRE
+          // '',// PAYS_SECONDAIRE
+          // '',// COMPTE_AUXILLIAIRE
+          // '',// RAISON_SOCIALE_ENTREPRISE
+          // '',// SIRET_ENTREPRISE
+          // '',// IDENTIFIANT_ENTREPRISE
+          '2025',// ANNEE
+          nomCentreEnseignement,// CENTRE_ENSEIGNEMENT
+          nomCentreAttachement,// CENTRE_ATTACHEMENT
+          // '',// STATUT_AUDITEUR
+          // '',// CENTRE_REGIONAL_INSCRIPTEUR
+          // '',// DIPLOME
+          // '',// SITUATION_GEOGRAPHIQUE
+          // '',// DISPOSITIF_FINANCEMENT
           '',// TYPE_FINANCEMENT_INSCRIPTION
-          '',// TYPE_TARIF_INSCRIPTION
-          '',// SITUATION_PROFESSIONNELLE
-          '',// STATUT_EMPLOI
-          '',// INSCRIT_POLE_EMPLOI
-          '',// COMPTE_ANALYTIQUE
+          // '',// TYPE_TARIF_INSCRIPTION
+          // '',// SITUATION_PROFESSIONNELLE
+          // '',// STATUT_EMPLOI
+          // '',// INSCRIT_POLE_EMPLOI
+          // '',// COMPTE_ANALYTIQUE
           nomFormation, // FORMATION
-          '',// ANNEE_FORMATION
-          '',// OPTION_FORMATION
+          // '',// ANNEE_FORMATION
+          // '',// OPTION_FORMATION
           nomGroupe, // GROUPE_FORMATION
           '',// STATUT_INSCRIPTION
           '',// TYPE_INSCRIPTION
-          '',// CODE_UNITE
-          '',// GROUPE_UNITE
-          '',// SEMESTRE_UNITE
-          '',// TYPE_FINANCEMENT_UNITE
-          '',// TYPE_TARIF_UNITE
-          '',// CENTRE_REGIONAL_ORGANISATEUR
-          '',// NOTE_SESSION_1
-          '',// NOTE_SESSION_2
-          '',// DATE_INSCRIPTION
+          // '',// CODE_UNITE
+          // '',// GROUPE_UNITE
+          // '',// SEMESTRE_UNITE
+          // '',// TYPE_FINANCEMENT_UNITE
+          // '',// TYPE_TARIF_UNITE
+          // '',// CENTRE_REGIONAL_ORGANISATEUR
+          // '',// NOTE_SESSION_1
+          // '',// NOTE_SESSION_2
+          // '',// DATE_INSCRIPTION
         ].map(v => `"${v !== undefined && v !== null ? v : ""}"`).join(";"));
       });
 
@@ -420,7 +232,7 @@ export default function ExportApprenants() {
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", `apprenants_groupe_${selectedGroupe}.csv`);
+      link.setAttribute("download", `import_Gescicca_groupe_${nomGroupe}.csv`);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -450,6 +262,54 @@ export default function ExportApprenants() {
         ))}
       </select>
 
+      <div>
+        <label>Nom exact de la formation dans Gescicca : </label>
+        <input
+          type="text"
+          value={nomFormation}
+          onChange={(e) => setNomFormation(e.target.value)}
+          className="export-input"
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <label>Centre d'enseignement : </label>
+        <select
+          onChange={(e) => setNomCentreEnseignement(e.target.value)}
+          value={nomCentreEnseignement}
+          className="export-select"
+        >
+          <option value="">Sélectionner un centre d'enseignement</option>
+          <option>Batiment Travaux Public</option>
+          <option>Informatique</option>
+          <option>Prévention des Risques, QSE</option>
+          <option>Industrie, Mécanique</option>
+          <option>Santé Action Sociale</option>
+          <option>Commerce Marketing</option>
+          <option>Ressources Humaines</option>
+          <option>Entrepreunariat Management Innovation</option>
+          <option>Logistique Transport</option>
+          <option>Gestion Comptabilite Finance</option>
+          <option>Droit ICH</option>
+        </select>
+      </div>
+
+      <div>
+        <label>Centre d'attachement : </label>
+        <select
+          onChange={(e) => setNomCentreAttachement(e.target.value)}
+          value={nomCentreAttachement}
+          className="export-select"
+        >
+          <option value="">Sélectionner un centre d'attachement</option>
+          <option>Amiens</option>
+          <option>Lille</option>
+          <option>Valenciennes</option>
+        </select>
+      </div>      
+      
       <button
         onClick={handleExport}
         disabled={loading || !selectedGroupe}
